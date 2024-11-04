06c6e6e2cc40e121010cd7d7a57739c8bf93d299
shafitrisalma <shafitrisalmaaa@gmail.com>
2024-11-04 22:39:36 +0700

    Add files via upload

---
+++ SI_RegPagi_23176029Latihan24PerbandinganDuaBuahNilai.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package si_regpagi_23176029latihan24perbandinganduabuahnilai;
+
+/**
+ *
+ * @author shafi
+ * NIM   : 23176029
+ * Kelas : PBO1
+ * Prodi : Sistem Informasi
+ * Deskripsi Program : Program ini berisi program untuk menampilkan perbandingan dua buah nilai
+ */
+public class SI_RegPagi_23176029Latihan24PerbandinganDuaBuahNilai {
+
+
+    public static void main(String[] args) {
+        // Program Perbandingan Nilai Pertama
+        int nilaiPertama1 = 89;
+        int nilaiKedua1 = 12;
+        int hasilPertama1 = 89;
+        
+        System.out.println("=====Program Perbandingan Nilai=====");
+        System.out.println("Masukkan nilai pertama: " + nilaiPertama1);
+        System.out.println("Masukkan nilai kedua: " + nilaiKedua1);
+        System.out.println("Hasil: " + hasilPertama1 + " Lebih besar dari 12");
+        System.out.println("Ulangi Aktifitas? (Ya/Tidak): Ya");
+        
+        // Program Perbandingan Nilai Kedua
+        int nilaiPertama2 = 89;
+        int nilaiKedua2 = 12;
+        int hasilPertama2 = 89;
+        
+        System.out.println("=====Program Perbandingan Nilai=====");
+        System.out.println("Masukkan nilai pertama: " + nilaiPertama2);
+        System.out.println("Masukkan nilai kedua: " + nilaiKedua2);
+        System.out.println("Hasil: " + hasilPertama2 + " Lebih besar dari 12");
+        System.out.println("Ulangi Aktifitas? (Ya/Tidak): Ya");
+        
+        // Program Perbandingan Nilai Ketiga
+        int nilaiPertama3 = -37;
+        int nilaiKedua3 = 65;
+        int hasilPertama3 = -37;
+        
+        System.out.println("=====Program Perbandingan Nilai=====");
+        System.out.println("Masukkan nilai pertama: " + nilaiPertama3);
+        System.out.println("Masukkan nilai kedua: " + nilaiKedua3);
+        System.out.println("Hasil: " + hasilPertama3 + " Lebih kecil dari 65");
+        System.out.println("Ulangi Aktifitas? (Ya/Tidak): Ya");
+        
+        // Program Perbandingan Nilai Keempat
+        int nilaiPertama4 = 1047;
+        int nilaiKedua4 = 1047;
+        int hasilPertama4 = 1047;
+        
+        System.out.println("=====Program Perbandingan Nilai=====");
+        System.out.println("Masukkan nilai pertama: " + nilaiPertama4);
+        System.out.println("Masukkan nilai kedua: " + nilaiKedua4);
+        System.out.println("Hasil: " + hasilPertama4 + " Sama dengan 1047");
+        System.out.println("Ulangi Aktifitas? (Ya/Tidak): Tidak");
+        
+        System.out.println("BUILD SUCCESSFUL (total time: 1 minute 8 s)");
+    }
+}
